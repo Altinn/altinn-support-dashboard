0381cb85ac6fdca0196a2b262434789a208989fb
Andreas Bjørnå <loguecow@protonmail.com>
2024-09-12 10:00:39 +0200

    unit tester for inputvalidering

--- backend/test/altinn-support-dashboard.backend.Tests/Controllers/AltinnApiControllerTests.cs
+++ backend/test/altinn-support-dashboard.backend.Tests/Controllers/AltinnApiControllerTests.cs
@@ -27,10 +27,55 @@ namespace altinn_support_dashboard.backend.Tests.Controllers
         [InlineData("12345678")]
         [InlineData("123456789333")]
         [InlineData("abcdefghij")]
-        public async Task GetRoles_ReturnsBadRequest_WhenOrgNumberLengthIsInvalid(string invalidOrgNumber)
+        public async Task GetOrganizationInfo_ReturnsBadRequest_WhenOrgNumberLengthIsInvalid(string invalidOrgNumber)
         {
             // Act
             var result = await _controller.GetOrganizationInfo(invalidOrgNumber);
+
+            // Assert
+            var badRequestResult = Assert.IsType<BadRequestObjectResult>(result);
+            Assert.Equal("Organisasjonsnummeret er ugyldig. Det må være 9 sifre langt.", badRequestResult.Value);
+        }
+
+        [Theory]
+        [InlineData(null)]
+        [InlineData("")]
+        public async Task GetOrganizationsByPhoneNumber_ReturnsBadRequest_WhenPhoneNumberLengthIsInvalid(string invalidPhonenumber)
+        {
+
+            // Act
+            var result = await _controller.GetOrganizationsByPhoneNumber(invalidPhonenumber);
+
+            // Assert
+            var badRequestResult = Assert.IsType<BadRequestObjectResult>(result);
+            Assert.Equal("Telefonnummeret er ugyldig. Det kan ikke være tomt.", badRequestResult.Value);
+        }
+        [Theory]
+        [InlineData(null)]
+        [InlineData("123")]
+        [InlineData("12345678")]
+        [InlineData("123456789333")]
+        [InlineData("abcdefghij")]
+        public async Task GetOrganizationsByEmail_ReturnsBadRequest_WhenEmailIsInvalid(string invalidEmail)
+        {
+            // Act
+            var result = await _controller.GetOrganizationsByEmail(invalidEmail);
+
+            // Assert
+            var badRequestResult = Assert.IsType<BadRequestObjectResult>(result);
+            Assert.Equal("E-postadressen er ugyldig.", badRequestResult.Value);
+        }
+        [Theory]
+        [InlineData(null)]
+        [InlineData("")]
+        [InlineData("123")]
+        [InlineData("12345678")]
+        [InlineData("123456789333")]
+        [InlineData("abcdefghij")]
+        public async Task GetPersonalContacts_ReturnsBadRequest_WhenOrgNumberLengthIsInvalid(string invalidOrgNumber)
+        {
+            // Act
+            var result = await _controller.GetPersonalContacts(invalidOrgNumber);
 
             // Assert
             var badRequestResult = Assert.IsType<BadRequestObjectResult>(result);
