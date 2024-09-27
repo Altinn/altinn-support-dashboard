7bcb38f654ea6861064ceeca8df5ad62ff50a249
Halsen <89974012+espenhalsen@users.noreply.github.com>
2024-09-27 12:06:06 +0200

    Implementering av automatiserte tester i frontend (#81)
    * Endret mappe struktur
    
    * installed vitest
    
    * ikke fungerende test, bare push
    
    * fixed test file and component
    
    * addded sidebarcomp test
    
    * MainContentComponent test
    
    * Model changes, nullable and required
    
    ---------
    
    Co-authored-by: Andreas Bjørnå <loguecow@protonmail.com>

--- frontend/altinn-support-dashboard.client/src/components/TopSearchBar/TopSearchBarComponent.tsx
+++ frontend/altinn-support-dashboard.client/src/components/TopSearchBar/TopSearchBarComponent.tsx
@@ -1,21 +1,33 @@
+// SearchComponent.tsx
+
 import React from 'react';
 import { Search } from '@digdir/designsystemet-react';
 import { MagnifyingGlassIcon } from '@navikt/aksel-icons';
 import '@digdir/designsystemet-theme';
 import '@digdir/designsystemet-css';
 
-const SearchComponent: React.FC<{ query: string, setQuery: (query: string) => void, handleSearch: () => void }> = ({ query, setQuery, handleSearch }) => (
+type SearchComponentProps = {
+    query: string;
+    setQuery: (query: string) => void;
+    handleSearch: () => void;
+};
+
+const SearchComponent: React.FC<SearchComponentProps> = ({ query, setQuery, handleSearch }) => (
     <div className="search-container">
-        <label htmlFor="query" className="search-label">Mobilnummer / E-post / Organisasjonsnummer:</label>
-        <form onSubmit={e => {
-            e.preventDefault();
-        }}>
+        <label htmlFor="searchbar" className="search-label">
+            Mobilnummer / E-post / Organisasjonsnummer:
+        </label>
+        <form
+            onSubmit={(e) => {
+                e.preventDefault();
+            }}
+        >
             <Search
                 id="searchbar"
-                label='Søk etter innhold'
-                clearButtonLabel=''
-                searchButtonLabel={<MagnifyingGlassIcon fontSize={'1.5em'} title='Search' />}
-                variant='primary'
+                label="Søk etter innhold"
+                clearButtonLabel=""
+                searchButtonLabel={<MagnifyingGlassIcon fontSize="1.5em" title="Search" />}
+                variant="primary"
                 value={query}
                 onChange={(e) => setQuery(e.target.value)}
                 onSearchClick={handleSearch}
