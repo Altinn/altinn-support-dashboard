1f7927bbc2513859d7a30bc4aaffa231740ebc5d
espenhalsen <espylv@gmail.com>
2024-10-21 12:11:51 +0200

    new fix to vite config

--- frontend/altinn-support-dashboard.client/vite.config.ts
+++ frontend/altinn-support-dashboard.client/vite.config.ts
@@ -10,6 +10,8 @@ import path from 'path';
 import child_process from 'child_process';
 import { env } from 'process';
 
+const isDevelopment = env.NODE_ENV === 'development';
+
 const baseFolder =
     env.APPDATA !== undefined && env.APPDATA !== ''
         ? `${env.APPDATA}/ASP.NET/https`
@@ -19,7 +21,7 @@ const certificateName = "altinn-support-dashboard.client";
 const certFilePath = path.join(baseFolder, `${certificateName}.pem`);
 const keyFilePath = path.join(baseFolder, `${certificateName}.key`);
 
-if (!fs.existsSync(certFilePath) || !fs.existsSync(keyFilePath)) {
+if (isDevelopment && (!fs.existsSync(certFilePath) || !fs.existsSync(keyFilePath))) {
     if (0 !== child_process.spawnSync('dotnet', [
         'dev-certs',
         'https',
@@ -33,7 +35,6 @@ if (!fs.existsSync(certFilePath) || !fs.existsSync(keyFilePath)) {
     }
 }
 
-// Fjerne ubrukt 'weatherforecast' proxy
 const target = env.ASPNETCORE_HTTPS_PORT ? `https://localhost:${env.ASPNETCORE_HTTPS_PORT}` :
     env.ASPNETCORE_URLS ? env.ASPNETCORE_URLS.split(';')[0] : 'https://localhost:7174';
 
@@ -47,10 +48,12 @@ export default defineConfig({
     },
     server: {
         port: 5173,
-        https: {
-            key: fs.readFileSync(keyFilePath),
-            cert: fs.readFileSync(certFilePath),
-        }
+        https: isDevelopment
+            ? {
+                key: fs.readFileSync(keyFilePath),
+                cert: fs.readFileSync(certFilePath),
+            }
+            : false, // Deaktiver HTTPS i produksjon
     },
     test: {
         globals: true,
