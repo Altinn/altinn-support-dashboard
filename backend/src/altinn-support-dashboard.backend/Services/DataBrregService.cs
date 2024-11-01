7da3f64e6726b84af95aa171db3263523f909a97
espenhalsen <espylv@gmail.com>
2024-11-01 13:31:08 +0100

    added all the brreg changes to branch

--- backend/src/altinn-support-dashboard.backend/Services/DataBrregService.cs
+++ backend/src/altinn-support-dashboard.backend/Services/DataBrregService.cs
@@ -1,27 +1,36 @@
 ﻿using altinn_support_dashboard.Server.Models;
 using altinn_support_dashboard.Server.Services.Interfaces;
 using altinn_support_dashboard.Server.Validation;
+using System;
+using System.Collections.Generic;
 using System.Text.Json;
+using System.Threading.Tasks;
 
 namespace altinn_support_dashboard.Server.Services
 {
     public class DataBrregService : IDataBrregService
     {
         private readonly DataBrregClient _client;
+        private readonly List<string> _validEnvironmentNames = new List<string> { "Production", "TT02" };
 
         public DataBrregService(DataBrregClient client)
         {
             _client = client;
         }
 
-        public async Task<ErRollerModel> GetRolesAsync(string orgNumber)
+        public async Task<ErRollerModel> GetRolesAsync(string orgNumber, string environmentName)
         {
-            if (string.IsNullOrWhiteSpace(orgNumber) || orgNumber.Length != 9 || !long.TryParse(orgNumber, out _))
+            if (string.IsNullOrWhiteSpace(orgNumber) || !ValidationService.IsValidOrgNumber(orgNumber))
             {
                 throw new ArgumentException("Organisasjonsnummeret er ugyldig. Det må være 9 sifre langt.");
             }
 
-            var result = await _client.GetRolesAsync(orgNumber);
+            if (!_validEnvironmentNames.Contains(environmentName))
+            {
+                throw new ArgumentException("Ugyldig miljønavn.");
+            }
+
+            var result = await _client.GetRolesAsync(orgNumber, environmentName);
             var rollerMain = JsonSerializer.Deserialize<ErRollerModel>(result, new JsonSerializerOptions
             {
                 PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
@@ -31,14 +40,19 @@ namespace altinn_support_dashboard.Server.Services
             return rollerMain;
         }
 
-        public async Task<UnderenhetRootObject> GetUnderenheter(string orgNumber)
+        public async Task<UnderenhetRootObject> GetUnderenheter(string orgNumber, string environmentName)
         {
             if (string.IsNullOrWhiteSpace(orgNumber) || !ValidationService.IsValidOrgNumber(orgNumber))
             {
                 throw new ArgumentException("Organisasjonsnummeret er ugyldig. Det må være 9 sifre langt.");
             }
 
-            var result = await _client.GetUnderenheter(orgNumber);
+            if (!_validEnvironmentNames.Contains(environmentName))
+            {
+                throw new ArgumentException("Ugyldig miljønavn.");
+            }
+
+            var result = await _client.GetUnderenheter(orgNumber, environmentName);
             var underenheter = JsonSerializer.Deserialize<UnderenhetRootObject>(result, new JsonSerializerOptions
             {
                 PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
