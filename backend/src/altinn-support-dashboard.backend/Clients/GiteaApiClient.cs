9c2b15dca3d08a884e89a96f679d7cfa3dba44d2
Andreas Bjørnå <loguecow@protonmail.com>
2025-06-12 10:23:54 +0200

    Created GiteaApiClient to test pat-token

---
+++ backend/src/altinn-support-dashboard.backend/Clients/GiteaApiClient.cs
@@ -0,0 +1,22 @@
+﻿public class GiteaApiClient
+{
+    private readonly HttpClient _httpClient;
+
+    public GiteaApiClient(HttpClient httpClient)
+    {
+        _httpClient = httpClient;
+    }
+
+    public async Task<string> GetCurrentUserAsync(string patToken, string giteaBaseUrl)
+    {
+        var request = new HttpRequestMessage(HttpMethod.Get, $"{giteaBaseUrl}/api/v1/user");
+        request.Headers.Add("Authorization", $"token {patToken}");
+
+        var response = await _httpClient.SendAsync(request);
+        if (!response.IsSuccessStatusCode)
+        {
+            return null;
+        }
+        return await response.Content.ReadAsStringAsync();
+    }
+}
