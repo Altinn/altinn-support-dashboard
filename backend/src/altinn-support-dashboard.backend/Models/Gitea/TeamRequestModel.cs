57a2a2c35fa6539de0c0aecde46332d8315ee85a
Andreas Bjørnå <loguecow@protonmail.com>
2025-06-13 13:45:57 +0200

    removed giteabaseurl requirement

--- backend/src/altinn-support-dashboard.backend/Models/Gitea/TeamRequestModel.cs
+++ backend/src/altinn-support-dashboard.backend/Models/Gitea/TeamRequestModel.cs
@@ -14,12 +14,6 @@ namespace AltinnSupportDashboard.Models.Gitea
         public string PatToken { get; set; }
         
         /// <summary>
-        /// The base URL of the Gitea instance
-        /// </summary>
-        [Required]
-        public string GiteaBaseUrl { get; set; }
-        
-        /// <summary>
         /// Team creation options
         /// </summary>
         [Required]
