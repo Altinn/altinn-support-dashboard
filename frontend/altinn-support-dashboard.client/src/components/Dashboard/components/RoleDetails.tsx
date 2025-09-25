4522bc6554fbb47defcfb58b3fd38a4b9769c1ca
Siri Berg-Johnsen <89572427+SiriBJ@users.noreply.github.com>
2025-09-25 08:30:27 +0200

    #208 Better descirption for roletypes (#228)
    * Made new file for handling the roletype cell, so that the name of the roles when they are external or local make more sense. put this into both manualrolesearchtable and roledetails
    
    * Update frontend/altinn-support-dashboard.client/src/components/RoleTypeCell.tsx

--- frontend/altinn-support-dashboard.client/src/components/Dashboard/components/RoleDetails.tsx
+++ frontend/altinn-support-dashboard.client/src/components/Dashboard/components/RoleDetails.tsx
@@ -13,6 +13,7 @@ import {
 import { useAppStore } from "../../../hooks/Appstore";
 import { useRoles } from "../../../hooks/hooks";
 import { PersonalContact } from "../models/mainContentTypes";
+import  RoleTypeCell  from "../../RoleTypeCell";
 
 interface RoleDetailsProps {
   selectedContact: PersonalContact;
@@ -47,7 +48,7 @@ export const RoleDetails: React.FC<RoleDetailsProps> = ({
         Tilbake til oversikt
       </Button>
 
-      <TableContainer component={Paper} sx={{ mb: 2 }}>
+      <TableContainer component={Paper} sx={{ mb: 2 }} >
         <MuiTable>
           <TableHead>
             <TableRow>
@@ -63,7 +64,7 @@ export const RoleDetails: React.FC<RoleDetailsProps> = ({
             {roleInfo && roleInfo.length > 0 ? (
               roleInfo.map((role, index) => (
                 <TableRow key={index}>
-                  <TableCell>{role.RoleType}</TableCell>
+                  <RoleTypeCell roleType = {role.RoleType} />
                   <TableCell>{role.RoleName}</TableCell>
                 </TableRow>
               ))
