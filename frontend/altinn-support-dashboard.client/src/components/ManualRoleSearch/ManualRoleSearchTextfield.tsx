2a541d708b97c5e3d118f5f71166a4cdf5d9aa47
SiriBJ <siriebj@gmail.com>
2025-09-30 13:35:25 +0200

    Switched to designsystemet for ManualRolSearchTextField

--- frontend/altinn-support-dashboard.client/src/components/ManualRoleSearch/ManualRoleSearchTextfield.tsx
+++ frontend/altinn-support-dashboard.client/src/components/ManualRoleSearch/ManualRoleSearchTextfield.tsx
@@ -1,44 +1,25 @@
 import React from "react";
-import { 
-    TextField,
-    InputAdornment,
-    Tooltip,
-    IconButton    
- } from "@mui/material";
- import HelpOutlineIcon from "@mui/icons-material/HelpOutline";
+ import { Textfield, Tooltip } from '@digdir/designsystemet-react';
 
 type ManualRoleSearchTextFieldProps = {
   label: string;
   value: string;
   onChange: (value: string) => void;
-  tooltip: string;
 }
     
 const ManualRoleSearchTextField: React.FC<ManualRoleSearchTextFieldProps> = ({
         label,
         value,
         onChange,
-        tooltip
     }) => {
     return (
-        <TextField
-            label={label}
-            variant="outlined"
-            value={value}
-            onChange={(e) => onChange(e.target.value)}
-            InputProps={{
-                endAdornment: (
-                    <InputAdornment position="end">
-                        <Tooltip title={tooltip}>
-                            <IconButton>
-                                <HelpOutlineIcon />
-                            </IconButton>
-                        </Tooltip>
-                    </InputAdornment>
-                )
-            }}
-            fullWidth
-        />
+        <>
+            <Textfield
+                label={label}
+                value={value}
+                onChange={(e) => onChange(e.target.value)}
+            />
+        </>
     );
 };
 
