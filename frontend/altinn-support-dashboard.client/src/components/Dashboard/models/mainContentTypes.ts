c324091104649421206efc1b1424cb04d6e821a2
Ugedal <fabian.ugedal@digdir.no>
2025-10-08 14:39:11 +0200

    fix: removed unused export

--- frontend/altinn-support-dashboard.client/src/components/Dashboard/models/mainContentTypes.ts
+++ frontend/altinn-support-dashboard.client/src/components/Dashboard/models/mainContentTypes.ts
@@ -1,12 +1,3 @@
-export interface fficialContact {
-  mobileNumber: string;
-  mobileNumberChanged: string;
-  eMailAddress: string;
-  eMailAddressChanged: string;
-  fratraadt?: boolean;
-  erDoed?: boolean;
-}
-
 export type SortDirection = "ascending" | "descending" | "none" | undefined;
 
 export type ERRolesSortField = "type" | "person" | "sistEndret" | null;
