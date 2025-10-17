a9583724176ed13986ff1306b9086e7dd8030d1f
SiriBJ <siriebj@gmail.com>
2025-10-17 10:01:37 +0200

    Made some css modules for the dialog box

--- frontend/altinn-support-dashboard.client/src/components/InformationDialog/InformationDialogContent.tsx
+++ frontend/altinn-support-dashboard.client/src/components/InformationDialog/InformationDialogContent.tsx
@@ -5,22 +5,24 @@ import {
     List
 } from "@digdir/designsystemet-react";
 import { CheckmarkIcon, XMarkIcon } from '@navikt/aksel-icons';
-
+import styles from "./styles/InformationDialogContent.module.css"
 
 
 const InformationDialogContent: React.FC = () => {
 
     return (
         <Dialog.Block>
-            <Paragraph>
+            <Paragraph className={styles.paragraph}>
                 Dette betyr at det er kunden som oppgir informasjon og du bekrefter om det stemmer eller ikke. Du skal ikke gi ut noen ny informasjon eller lese opp.
             </Paragraph>
-            <List.Unordered>
+            <List.Unordered className={styles.listContainer}>
                 <List.Item>
-                    <Paragraph>
+                    <Paragraph className={styles.listParagraph}>
                         Kunden sier adressen og du bekrefter eller avkrefter at den er riktig 
                     </Paragraph>
-                    <CheckmarkIcon title="Bekreft ikon" />
+                    <CheckmarkIcon title="Bekreft ikon" 
+                    className={styles.listIcon}
+                    />
                 </List.Item>
                 <List.Item>
                     <Paragraph>
