70d1607aece75a741c5b30097e09d52e946560d5
SiriBJ <siriebj@gmail.com>
2025-10-24 12:30:48 +0200

    removed the toggle and fixed the white logo

--- frontend/altinn-support-dashboard.client/src/pages/SignInPage.tsx
+++ frontend/altinn-support-dashboard.client/src/pages/SignInPage.tsx
@@ -4,14 +4,13 @@ import {
     Heading
 } from '@digdir/designsystemet-react';
 import style from './styles/SignInPage.module.css';
-import logo from '../assets/Logo-logIn.png'
+import logo from '../assets/log-in-white.png'
 import coloredLogo from "../assets/log-in-blue.png";
 import { useAppStore } from "../stores/Appstore";
 
 
 export const SignInPage: React.FC = () => {
     const isDarkMode = useAppStore((state) => state.isDarkMode)
-    const toggleDarkMode = useAppStore((state) => state.setIsDarkMode)
     return (
         <div>
             <img 
@@ -24,10 +23,6 @@ export const SignInPage: React.FC = () => {
             <Button variant="primary">
                 Logg inn med AI-dev
             </Button>
-
-            <Button 
-            onClick={() => toggleDarkMode(!isDarkMode)} 
-            variant="secondary">Toggle Dark Mode</Button>
         </div>
     )
 }
