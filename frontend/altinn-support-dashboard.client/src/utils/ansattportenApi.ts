78b5bc0a7171aad5e271c1e2240267916ea12b24
Ugedal <fabian.ugedal@digdir.no>
2025-11-07 11:04:27 +0100

    feat: fixed small merge issue

--- frontend/altinn-support-dashboard.client/src/utils/ansattportenApi.ts
+++ frontend/altinn-support-dashboard.client/src/utils/ansattportenApi.ts
@@ -5,13 +5,9 @@ export const fetchAuthDetails = async (): Promise<authDetails> => {
   try {
     const res = await authorizedFetch(`${getBaseUrl()}/auth/auth-status`);
 
-  if (!res.ok) {
-    return { isLoggedIn: false, name: "", ansattportenActive: true };
-  }
-
-  const data = await res.json();
-  const authDetails = data as authDetails;
-
+    if (!res.ok) {
+      return { isLoggedIn: false, name: "", ansattportenActive: true };
+    }
     const data = await res.json();
     return data as authDetails;
   } catch (err) {
