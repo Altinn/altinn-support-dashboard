90cbdf317401a281e145bd3c40b11640a4abd3a4
Ugedal <fabian.ugedal@digdir.no>
2025-11-14 09:15:37 +0100

    chore:r emoved console.writlines

--- backend/src/altinn-support-dashboard.backend/Services/AltinnApiService.cs
+++ backend/src/altinn-support-dashboard.backend/Services/AltinnApiService.cs
@@ -44,11 +44,9 @@ namespace altinn_support_dashboard.Server.Services
 
 
             //strips country codes plus the two digits after when searching
-            Console.WriteLine(phoneNumber);
             phoneNumber = phoneNumber.Trim();
             string strippedPhoneNumber = Regex.Replace(phoneNumber, @"^\+\d{1,2}", "");
 
-            Console.WriteLine(strippedPhoneNumber);
 
             var result = await _client.GetOrganizationsByPhoneNumber(strippedPhoneNumber, environment);
             var organizations = JsonSerializer.Deserialize<List<OrganizationByPhoneMail>>(result, new JsonSerializerOptions
