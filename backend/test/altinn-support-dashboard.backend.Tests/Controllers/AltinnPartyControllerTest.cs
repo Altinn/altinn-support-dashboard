87858b547bf56ff8172fa73ea151c3caa5314eea
SiriBJ <siriebj@gmail.com>
2025-12-16 10:18:36 +0100

    Added tests for GetPartySsn and GetPartyRoles

--- backend/test/altinn-support-dashboard.backend.Tests/Controllers/AltinnPartyControllerTest.cs
+++ backend/test/altinn-support-dashboard.backend.Tests/Controllers/AltinnPartyControllerTest.cs
@@ -51,4 +51,67 @@ public class AltinnPartyControllerTests
         Assert.Equal(500, objectResult?.StatusCode);
     }
 
+    [Fact]
+    public async Task GetPartySsn_ReturnsResult_WhenSsnIsValid()
+    {
+        var validSsn = "11111111111";
+        var expectedParty = new PartyModel
+        {
+            PartyUuid = "uuid-ssn",
+            Ssn = validSsn,
+            Name = "Test Person"
+        };
+
+        _mockPartyApiService
+        .Setup(service => service.GetPartyFromSsnAsync(validSsn))
+        .ReturnsAsync(expectedParty);
+
+        var result = await _controller.GetPartySsn(validSsn);
+
+        Assert.IsType<OkObjectResult>(result);
+    }
+
+    [Fact]
+    public async Task GetPartySsn_Returns500_WhenServiceThrowsException()
+    {
+        _mockPartyApiService
+        .Setup(service => service.GetPartyFromSsnAsync(It.IsAny<string>()))
+        .ThrowsAsync(new Exception("Service error"));
+
+        var result = await _controller.GetPartySsn("");
+
+        Assert.IsType<ObjectResult>(result);
+        var objectResult = result as ObjectResult;
+        Assert.Equal(500, objectResult?.StatusCode);
+    }
+
+    [Fact]
+    public async Task GetPartyRoles_ReturnsResult_WhenUuidIsValid()
+    {
+        var validUuid = "11111111-1111-1111-1111-111111111111";
+        var expectedRolesJson = "{\"roles\": [\"role1\", \"role2\"]}";
+
+        _mockPartyApiService
+        .Setup(service => service.GetRolesFromPartyAsync(validUuid))
+        .ReturnsAsync(expectedRolesJson);
+
+        var result = await _controller.GetPartyRoles(validUuid);
+
+        Assert.IsType<OkObjectResult>(result);
+    }
+
+    [Fact]
+    public async Task GetPartyRoles_Returns500_WhenServiceThrowsException()
+    {
+        _mockPartyApiService
+        .Setup(service => service.GetRolesFromPartyAsync(It.IsAny<string>()))
+        .ThrowsAsync(new Exception("Service error"));
+
+        var result = await _controller.GetPartyRoles("invalid-uuid");
+
+        Assert.IsType<ObjectResult>(result);
+        var objectResult = result as ObjectResult;
+        Assert.Equal(500, objectResult?.StatusCode);
+    }
+
 }
