8234ee80ee9170635aaa27aa6444bb7fad599729
SiriBJ <siriebj@gmail.com>
2026-01-12 13:02:59 +0100

    Made the page

---
+++ frontend/altinn-support-dashboard.client/src/pages/CorrespondencePage.tsx
@@ -0,0 +1,16 @@
+import { Heading } from "@digdir/designsystemet-react"
+
+
+
+
+
+export const CorrespondencePage = () => {
+
+    return (
+        <div>
+            <Heading level={1} data-size="sm">
+                Opprett melding for test
+            </Heading>
+        </div>
+    )
+}
