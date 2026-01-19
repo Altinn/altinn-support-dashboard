f3155dd3e7ed8182051c4e3e6087390d43c3f531
Fabian Ugedal <115708173+ugedal1@users.noreply.github.com>
2026-01-19 07:31:22 +0100

    496 setup correspondence api endpoint backend (#502)
    * feat: request models made
    
    Request models are made, a few still might need to be updated since they
    might be of a different object than string
    
    * feat: finalized correspondence models
    
    * feat: made client
    
    * chore
    
    * feat: setup Correspondece service
    
    * feat: setup CorrespondenceController
    
    * feat: correspondence client now works as intended
    
    * chore: small fixes
    
    * feat: Attachments added
    
    * feat: added isConfirmed field
    
    * chore: made summary for controller
    
    * feat: added recipient check for service
    
    * feat: added tests for service
    
    * chore: addet comment
    
    * feat: changed ssntokenservice tests to take less time
    
    * feat: added extended validation for correspondence
    
    * feat: updated tests
    
    ---------
    
    Co-authored-by: ugedal1 <fabian.ugedal@digdir.no>

---
+++ backend/src/altinn-support-dashboard.backend/Clients/CorrespondenceClient.cs
@@ -0,0 +1,97 @@
+
+
+using System.Net.Http.Headers;
+using System.Text;
+using System.Text.Json;
+using altinn_support_dashboard.Server.Models;
+using altinn_support_dashboard.Server.Models.correspondence;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+
+public class CorrespondenceClient : ICorrespondenceClient
+{
+    private readonly HttpClient _client;
+    private readonly ILogger<ICorrespondenceClient> _logger;
+    public CorrespondenceClient(IHttpClientFactory _clientFactory, IOptions<Configuration> configuration, ILogger<ICorrespondenceClient> logger)
+    {
+        _client = _clientFactory.CreateClient("TT02");
+        _client.BaseAddress = new Uri(configuration.Value.TT02.BaseAddressAltinn3);
+        _client.Timeout = TimeSpan.FromSeconds(configuration.Value.TT02.Timeout);
+        _client.DefaultRequestHeaders.Add("ApiKey", configuration.Value.TT02.ApiKey);
+        _logger = logger;
+
+    }
+
+    //Creates a correspondence with an attachment
+    public async Task<string> UploadCorrespondence(CorrespondenceUploadRequest request)
+    {
+        string requestUrl = "correspondence/api/v1/correspondence/upload";
+
+
+        // expects a flattened format
+        var form = new MultipartFormDataContent();
+
+        // Correspondence required fields
+        form.Add(new StringContent(request.Correspondence.ResourceId), "correspondence.resourceid");
+        form.Add(new StringContent(request.Correspondence.SendersReference), "correspondence.sendersreference");
+        form.Add(new StringContent(request.Correspondence.Content.Language), "correspondence.content.language");
+        form.Add(new StringContent(request.Correspondence.Content.MessageTitle), "correspondence.content.messagetitle");
+        form.Add(new StringContent(request.Correspondence.Content.MessageBody), "correspondence.content.messagebody");
+
+        // Correspondence optional fields
+        AddIfNotNull(form, request.Correspondence.IsConfirmedNeeded.ToString(), "correspondence.isconfirmation needed");
+
+
+        AddIfNotNull(form, request.Correspondence.Content.MessageSummary, "correspondence.content.messageSummary");
+
+
+        // Recipients
+        for (int i = 0; i < request.Recipients.Count; i++)
+        {
+            form.Add(new StringContent(request.Recipients[i]), $"recipients[{i}]");
+        }
+
+
+
+
+        //Attachments
+        //In the future we might add the ability to upload custom attachments
+        var filestream = File.OpenRead("test.txt");
+        var fileContent = new StreamContent(filestream);
+        fileContent.Headers.ContentType = new MediaTypeHeaderValue("application/octet-stream");
+        form.Add(fileContent, "Attachments", "testfile.txt");
+        form.Add(
+            new StringContent("testfile-1"),
+            "correspondence.content.attachments[0].sendersReference"
+        );
+
+        form.Add(
+            new StringContent("testfile.txt"),
+            "correspondence.content.attachments[0].filename"
+        );
+
+
+        var response = await _client.PostAsync(requestUrl, form);
+        if (response.IsSuccessStatusCode)
+        {
+            return await response.Content.ReadAsStringAsync();
+        }
+        else
+        {
+            var responseBody = await response.Content.ReadAsStringAsync();
+            throw new Exception($"API request failed with status code {response.StatusCode}: {responseBody}");
+
+        }
+
+    }
+
+    //helper function for all optional fields
+    private static void AddIfNotNull(MultipartFormDataContent form, string? value, string name)
+    {
+
+        if (!string.IsNullOrEmpty(value))
+        {
+            form.Add(new StringContent(value), name);
+        }
+    }
+}
