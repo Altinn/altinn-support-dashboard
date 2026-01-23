38c84c8c3064353dcfd1270b1851018f10fabcb3
SiriBJ <siriebj@gmail.com>
2026-01-23 15:53:15 +0100

    Added small comment about last change

--- frontend/altinn-support-dashboard.client/test/Correspondence/CorrespondenceButton.test.tsx
+++ frontend/altinn-support-dashboard.client/test/Correspondence/CorrespondenceButton.test.tsx
@@ -1,6 +1,7 @@
 /**
  * @vitest-environment jsdom
  */
+//Had to add the above line to make sure the test runs in our pipline
 import { describe, expect, vi } from "vitest";
 import { render, screen } from "@testing-library/react";
 import { userEvent } from "@testing-library/user-event/dist/cjs/setup/index.js";
