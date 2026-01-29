a105c26b537cc23cf8a36cf8e5d47fefe72d16f5
SiriBJ <siriebj@gmail.com>
2026-01-29 13:50:34 +0100

    Started on the switch

--- frontend/altinn-support-dashboard.client/src/components/Dashboard/components/contacts/NotificationContactCell.tsx
+++ frontend/altinn-support-dashboard.client/src/components/Dashboard/components/contacts/NotificationContactCell.tsx
@@ -1,22 +1,32 @@
 import { useDashboardStore } from "../../../../stores/DashboardStore";
 import { Table } from "@digdir/designsystemet-react";
 import style from "../../styles/NotificationContactTable.module.css";
+import { NotificationAdresses } from "../../../../models/models";
 
 interface NotificationContactCellProps {
-  contact: string | null;
+  contact: NotificationAdresses;
+  field: keyof NotificationAdresses;
 }
 
 const NotificationContactCell: React.FC<NotificationContactCellProps> = ({
   contact,
+  field,
 }) => {
   const userInput = useDashboardStore((s) => s.query.replace(/\s/g, "").toLowerCase());
 
+  const value = contact[field] as string | null;
+
+  //Add countrycode if phonenumber
+  const displayValue = field === "phone" && value
+    ? `${contact.countryCode || ""} ${value}`.trim()
+    : value;
+
   //outlines if searchquery is part of the cell
-  const boldened = contact === userInput ? { fontWeight: "bold" } : undefined;
+  const boldened = value === userInput ? { fontWeight: "bold" } : undefined;
 
   return (
     <Table.Cell className={style["cellText"]} style={boldened}>
-      {contact || "-"}
+      {displayValue || "-"}
     </Table.Cell>
   );
 };
