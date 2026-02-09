7949837e129e0a6e7366435231187befd034be4c
ugedal1 <ugedal1@hotmail.com>
2026-02-09 17:48:25 +0100

    Revert "542 connect new organizationsearch endpoint to frontend (#543)"
    This reverts commit 182be574aa3f22790f7d6a3d11f597ebdab9b320.

--- backend/src/altinn-support-dashboard.backend/Services/Altinn3Service.cs
+++ backend/src/altinn-support-dashboard.backend/Services/Altinn3Service.cs
@@ -3,7 +3,6 @@ using altinn_support_dashboard.Server.Services.Interfaces;
 using altinn_support_dashboard.Server.Utils;
 using Microsoft.AspNetCore.Http.Json;
 using Microsoft.Extensions.Compliance.Redaction;
-using Microsoft.IdentityModel.Tokens;
 using Models.altinn3Dtos;
 using System.Text.Json;
 using System.Text.RegularExpressions;
@@ -44,13 +43,11 @@ public class Altinn3Service : IAltinn3Service
         var json = await _client.GetOrganizationInfo(orgNumber, environment);
         var result = JsonSerializer.Deserialize<PartyNamesResponseDto>(json, jsonOptions);
 
-        if (result == null || result.PartyNames[0].Name.IsNullOrEmpty())
+
+        if (result == null)
         {
             throw new Exception($"No data found for org with orgnumber: {orgNumber}");
         }
-
-
-
         return result.PartyNames[0];
     }
 
@@ -58,7 +55,6 @@ public class Altinn3Service : IAltinn3Service
     public async Task<Organization> GetOrganizationByOrgNoAltinn3(string orgNumber, string environment)
     {
         PartyNameDto partyName = await GetOrganizationPartyNameAltinn3(orgNumber, environment);
-
 
         var organization = new Organization
         {
