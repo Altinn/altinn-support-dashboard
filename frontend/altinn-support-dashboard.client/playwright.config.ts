19bf9ce9f7129112113b2164b39e003330ca52ac
SiriBJ <siriebj@gmail.com>
2026-03-02 13:52:29 +0100

    set up for e2e testing

---
+++ frontend/altinn-support-dashboard.client/playwright.config.ts
@@ -0,0 +1,19 @@
+import { defineConfig } from '@playwright/test';
+
+export default defineConfig({
+  testDir: './e2e',
+  fullyParallel: true,
+  forbidOnly: !!process.env.CI,
+  retries: process.env.CI ? 2 : 0,
+  workers: process.env.CI ? 1 : undefined,
+  reporter: 'html',
+  use: {
+    baseURL: "http:localhost:5173",
+    trace: 'on-first-retry',
+  },
+  webServer: {
+    command: 'npm run dev',
+    url: 'http://localhost:5173',
+    reuseExistingServer: !process.env.CI,
+  },
+});
