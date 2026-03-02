cb396e189cd89c76d4dd4c8b17ee99189ec1d711
Fabian Ugedal <115708173+ugedal1@users.noreply.github.com>
2026-03-02 07:38:28 +0100

    feat: Ability to send correspondence to selfidentified users (#617)
    * feat: added new selfidentified resource to backend
    
    * feat:selfidentified users can get correspondece
    
    - this can be done through email, will get 404 if no user connected to
      email
    
    * fix: test
    
    * fix: tests

--- backend/test/altinn-support-dashboard.backend.Tests/Services/AltinnApiServiceTest.cs
+++ backend/test/altinn-support-dashboard.backend.Tests/Services/AltinnApiServiceTest.cs
@@ -53,7 +53,7 @@ public class AltinnApiServiceTest
                 MaskinportenSettings = new MaskinportenSettings
                 { }
             },
-            Correspondence = new CorrespondenceResourceType { DefaultResourceId = "default", ConfidentialityResourceId = "confident" }
+            Correspondence = new CorrespondenceResourceType { DefaultResourceId = "default", ConfidentialityResourceId = "confident", SelfIdentifiedResourceId = "self" }
         });
         mockHttpClient.Setup(x => x.CreateClient(It.IsAny<string>()))
             .Returns(new HttpClient());
