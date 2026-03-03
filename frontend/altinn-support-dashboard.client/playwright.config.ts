d7d5ac16ac8983edc23b6507d9cd63261d0e9c67
Siri Berg-Johnsen <89572427+SiriBJ@users.noreply.github.com>
2026-03-03 14:35:01 +0100

    619 create e2e test for manualrolesearch page (#620)
    * set up for e2e testing
    
    * started on the test file for manualroleserach page
    
    * made e2e tests for manualrole search. added in the page, a test-id for the button bc it could not be found without it, added ansattporten: false for development
    
    * removed an example file playwright made itself. made a fixtures file, that mocks the authentication so the tests can call backend/the other pages
    
    * Made changes to frontend-ci.yml so it will run playwright tests too
    
    * tried to fix tests by excluding e2e from vitest
    
    * tried to fix playwright tests by changing config
    
    * Made changes to config that checks if it is local or CI trying to run playwright and changes the baseURl accordingly
    
    * Made changes so it will make a comment that shows which tests fail/pass in the PR
    
    * small typo
    
    * removed comment fucntion, bc i didn't like the look

---
+++ frontend/altinn-support-dashboard.client/playwright.config.ts
@@ -0,0 +1,25 @@
+import { defineConfig } from '@playwright/test';
+
+const isCI = !!process.env.CI;
+const baseURL = isCI ? "http://localhost:5173" : "https://localhost:5173";
+
+export default defineConfig({
+  testDir: './e2e',
+  fullyParallel: true,
+  forbidOnly: isCI,
+  retries: isCI ? 2 : 0,
+  workers: isCI ? 1 : undefined,
+  reporter: 'html',
+  use: {
+    baseURL: baseURL,
+    ignoreHTTPSErrors: true,
+    trace: 'on-first-retry',
+  },
+  webServer: {
+    command: 'npx vite --host',
+    url: baseURL,
+    ignoreHTTPSErrors: true,
+    reuseExistingServer: !isCI,
+    ...(isCI && { env: { NODE_ENV: 'test' } }),
+  },
+});
