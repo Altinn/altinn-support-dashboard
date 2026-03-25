be52ccb070aa98e1457e175b7a3c3af612aaa09d
ugedal1 <ugedal1@hotmail.com>
2026-03-25 10:57:07 +0100

    chore: changed routing

--- frontend/altinn-support-dashboard.client/src/app/PrivateRoutes.tsx
+++ frontend/altinn-support-dashboard.client/src/app/PrivateRoutes.tsx
@@ -1,15 +1,15 @@
 import { Navigate, Outlet } from "react-router-dom";
-import { useAuthDetails } from "../hooks/ansattportenHooks";
-import { Spinner } from "@digdir/designsystemet-react";
 
 const PrivateRoutes = () => {
-  const authDetails = useAuthDetails();
+  // const authDetails = useAuthDetails();
+  //
+  // if (authDetails.isLoading) {
+  //   return <Spinner aria-label="authorizing" data-size="xl" />;
+  // }
+  // return authDetails?.data?.isLoggedIn ? <Outlet /> : <Navigate to="/signin" />;
+  const isLoggedInList = (window.location.href = `/.auth/me`);
 
-  if (authDetails.isLoading) {
-    return <Spinner aria-label="authorizing" data-size="xl" />;
-  }
-
-  return authDetails?.data?.isLoggedIn ? <Outlet /> : <Navigate to="/signin" />;
+  return isLoggedInList.length <= 0 ? <Outlet /> : <Navigate to="/signin" />;
 };
 
 export default PrivateRoutes;
