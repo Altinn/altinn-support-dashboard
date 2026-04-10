511a8c06a736455b1f6b23cb3f3275595d2c22d1
SiriBJ <siriebj@gmail.com>
2026-04-10 12:43:49 +0200

    Maded the route for the new notification page

---
+++ frontend/altinn-support-dashboard.client/src/pages/NotificationPage.tsx
@@ -0,0 +1,14 @@
+import { Heading } from '@digdir/designsystemet-react';
+
+
+
+
+export const NotificationPage = () => {
+    return (
+        <div>
+            <Heading level={1} data-size="sm">
+                Søk etter varsling
+            </Heading>
+        </div>
+    )
+}
