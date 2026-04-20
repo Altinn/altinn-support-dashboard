f71c8e1fbdaf19869dd1aad4560d032b10ff0cf3
SiriBJ <siriebj@gmail.com>
2026-04-20 08:53:34 +0200

    made change to colormap

--- frontend/altinn-support-dashboard.client/src/components/Notification/NotificationStatusCode.tsx
+++ frontend/altinn-support-dashboard.client/src/components/Notification/NotificationStatusCode.tsx
@@ -7,19 +7,40 @@ type NotificationStatusCodeProps = {
     notification: NotificationItem;
 }
 
+const colorMap: Record<string, "success" | "danger" | "warning" | "info"> = {
+    registered: "info",
+    processing: "info",
+    completed: "success",
+    sendconditionnotmet: "danger",
+    cancelled: "danger",
+    processed: "warning",
+    new: "info",
+    sending: "info",
+    accepted: "warning",
+    delivered: "success",
+    failed: "danger",
+    failed_invalidrecipient: "danger",
+    failed_recipientreserved: "danger",
+    failed_barredreceiver: "danger",
+    failed_deletedreceiver: "danger",
+    failed_expired: "danger",
+    failed_undelivered: "danger",
+    failed_recipientnotidentified: "danger",
+    failed_rejected: "danger",
+    succeeded: "success",
+    failed_invalidformat: "danger",
+    failed_suppressedrecipient: "danger",
+    failed_transienterror: "danger",
+    failed_bounced: "danger",
+    failed_filterspam: "danger",
+    failed_quarantined: "danger",
+    failed_ttl: "danger",
+
+}
+
+const getColor = (status: string) => colorMap[status.toLowerCase()] || "danger";
+
 const NotificationStatusCode: React.FC<NotificationStatusCodeProps> = ({ notification }) => {
-    const getColor = (status: string) => {
-        switch (status?.toLowerCase()) {
-            case "delivered":
-                return "success";
-            case "failed":
-                return "danger";
-            case "new":
-                return "warning";
-            default:
-                return "info";
-        }
-    };
 
     return (
         <Alert data-color={getColor(notification.sendStatus?.status)} data-size="sm" className={style.alert}>
