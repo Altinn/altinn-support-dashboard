7a4e4533dbcc84c5d4adef0cd4b63f7d4822e98c
ugedal1 <ugedal1@hotmail.com>
2026-04-20 09:57:09 +0200

    feat: changed instance dto

--- backend/src/altinn-support-dashboard.backend/Models/altinn3/RolesAndRightsDto.cs
+++ backend/src/altinn-support-dashboard.backend/Models/altinn3/RolesAndRightsDto.cs
@@ -6,5 +6,12 @@ public class RolesAndRightsDto
     public string[]? AuthorizedAccessPackages { get; set; }
     public List<string>? AuthorizedResources { get; set; }
     public List<string>? AuthorizedRoles { get; set; }
-    public string[]? AuthorizedInstances { get; set; }
+    public List<AuthorizedInstanceDto>? AuthorizedInstances { get; set; }
+}
+
+public class AuthorizedInstanceDto
+{
+    public string? ResourceId { get; set; }
+    public string? InstanceId { get; set; }
+    public string? InstanceRef { get; set; }
 }
