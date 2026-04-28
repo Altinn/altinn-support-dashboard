647c70340e7741a208cdb82026742193d2a164af
ugedal1 <ugedal1@hotmail.com>
2026-04-28 09:15:16 +0200

    chore: small role name change

--- backend/src/altinn-support-dashboard.backend/Security/AzureAuthorizationExtensions.cs
+++ backend/src/altinn-support-dashboard.backend/Security/AzureAuthorizationExtensions.cs
@@ -10,7 +10,7 @@ public static class AzureRoles
 {
     public const string Authenticated = "AzureAuthenticated";
     public const string TT02 = "Dashboard.TT02";
-    public const string Production = "Dashboard.Production";
+    public const string Production = "Dashboard.PROD";
 }
 
 public static class AzureAuthorizationExtensions
