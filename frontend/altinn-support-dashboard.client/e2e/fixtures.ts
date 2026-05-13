e72f3107c21878a8b71671cc625f5bc0470ac4a7
ugedal1 <ugedal1@hotmail.com>
2026-05-13 13:00:36 +0200

    chore: fixed playwright tests

--- frontend/altinn-support-dashboard.client/e2e/fixtures.ts
+++ frontend/altinn-support-dashboard.client/e2e/fixtures.ts
@@ -2,15 +2,15 @@ import {test as base, expect} from "@playwright/test";
 
 export const test = base.extend({
     page: async ({ page }, use) => {
-        await page.route('**/auth/auth-status', async (route) => {
+        await page.route('**/azure-auth/auth-status', async (route) => {
             await route.fulfill({
                 status: 200,
                 contentType: 'application/json',
                 body: JSON.stringify({
                     isLoggedIn: true,
                     name: "Test User",
-                    orgName: "Test Org",
-                    userPolicies: []
+                    azureAuthActive: true,
+                    roles: []
                 }),
             });
         });
