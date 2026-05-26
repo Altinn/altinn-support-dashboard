b25a84f179e9d20218ce3faaa3bb03480a12d583
ugedal1 <ugedal1@hotmail.com>
2026-05-26 09:53:01 +0200

    feat: added more visible errors which is sent from backend

--- backend/src/altinn-support-dashboard.backend/Controllers/NotificationsController.cs
+++ backend/src/altinn-support-dashboard.backend/Controllers/NotificationsController.cs
@@ -1,4 +1,5 @@
 using altinn_support_dashboard.Server.Services.Interfaces;
+using altinn_support_dashboard.Server.Utils;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Security;
@@ -12,6 +13,8 @@ namespace AltinnSupportDashboard.Controllers;
 [Authorize(AzureRoles.CoreInternal)]
 public class NotificationsController : ControllerBase
 {
+    private const string InvalidOrderIdMessage = "Ordre-ID is invalid. It should be in GUID format";
+
     private readonly INotificationsService _service;
 
     public NotificationsController(INotificationsService service)
@@ -22,6 +25,11 @@ public class NotificationsController : ControllerBase
     [HttpGet("orderid/email/{orderId}")]
     public async Task<IActionResult> GetEmailNotificationsByOrderId(string orderId)
     {
+        if (!ValidationService.IsValidNotificationOrderId(orderId))
+        {
+            return BadRequest(InvalidOrderIdMessage);
+        }
+
         var response = await _service.GetEmailNotificationsByOrderId(orderId);
         return Ok(response);
     }
@@ -29,6 +37,11 @@ public class NotificationsController : ControllerBase
     [HttpGet("orderid/sms/{orderId}")]
     public async Task<IActionResult> GetSmsNotificationsByOrderId(string orderId)
     {
+        if (!ValidationService.IsValidNotificationOrderId(orderId))
+        {
+            return BadRequest(InvalidOrderIdMessage);
+        }
+
         var response = await _service.GetSmsNotificationsByOrderId(orderId);
         return Ok(response);
     }
@@ -36,6 +49,11 @@ public class NotificationsController : ControllerBase
     [HttpGet("orderid/{orderId}")]
     public async Task<IActionResult> GetAllNotificationsByOrderId(string orderId)
     {
+        if (!ValidationService.IsValidNotificationOrderId(orderId))
+        {
+            return BadRequest(InvalidOrderIdMessage);
+        }
+
         var response = await _service.GetAllNotificationsByOrderId(orderId);
         return Ok(response);
     }
