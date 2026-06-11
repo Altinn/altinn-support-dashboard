26f5b905b0a7b50b4bad9c8d5c00bf6e90499ba8
SiriBJ <siriebj@gmail.com>
2026-06-11 12:30:46 +0200

    Added a css module for the resource page

--- frontend/altinn-support-dashboard.client/src/pages/ResourceSearchPage.tsx
+++ frontend/altinn-support-dashboard.client/src/pages/ResourceSearchPage.tsx
@@ -3,28 +3,35 @@ import { useState } from "react"
 import { ResourceSearchResult } from "../models/resourceModels";
 import {ResourceSearchSearchBar} from "../components/ResourceSearch/ResourceSearchSearchBar";
 import { ResourceSearchList } from "../components/ResourceSearch/ResourceSearchList";
-
-
-
-
-
+import styles from "./styles/ResourceSearchPage.module.css"
 
 
 export const ResourceSearchPage = () => {
-    const [query, setQuery] = useState("");
-    const [selectedResource, setSelectedResource] =
-        useState<ResourceSearchResult | null>(null)
-    return (
-        <div>
-            <Heading level={1} data-size="sm">
-                Søk etter ressurser
-            </Heading>
-            <ResourceSearchSearchBar query={query} setQuery={(setQuery)} setSelectedResource={setSelectedResource} />
-            <ResourceSearchList
-                query={query}
-                selectedResource={selectedResource}
-                setSelectedResource={setSelectedResource}
-            />
+  const [query, setQuery] = useState("");
+  const [selectedResource, setSelectedResource] =
+    useState<ResourceSearchResult | null>(null);
+
+  return (
+    <div className={styles.pageContainer}>
+      <Heading level={1} data-size="sm">
+        Søk etter ressurser
+      </Heading>
+      <ResourceSearchSearchBar
+        query={query}
+        setQuery={setQuery}
+        setSelectedResource={setSelectedResource}
+      />
+      <div className={styles.mainContainer}>
+        <div className={styles.listContainer}>
+          <ResourceSearchList
+            query={query}
+            selectedResource={selectedResource}
+            setSelectedResource={setSelectedResource}
+          />
         </div>
-    )
-}
+        <div className={styles.detailedContainer}>
+        </div>
+      </div>
+    </div>
+  );
+};
