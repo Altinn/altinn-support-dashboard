edcf42d2a9ee99973aa7bbfd41cf0e7ed04442fa
SiriBJ <siriebj@gmail.com>
2026-06-19 07:59:48 +0200

    started on a new card

---
+++ frontend/altinn-support-dashboard.client/src/components/Notification/NIN search/NotificationShipmentCard.tsx
@@ -0,0 +1,14 @@
+import { NotificationShipmentResponse } from "../../../models/notificationModels"
+
+
+
+
+type NotificationShipemntCardProps = {
+    shipment: NotificationShipmentResponse
+};
+
+const NotificationShipmentCard: React.FC<NotificationShipemntCardProps> = ({
+    shipment
+}) => (
+    
+)
