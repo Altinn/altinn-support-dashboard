44af972ac2eddb46b64ea8e962de220b20d01658
SiriBJ <siriebj@gmail.com>
2026-06-23 06:38:55 +0200

    Started on the changes for notification page

--- frontend/altinn-support-dashboard.client/src/components/Notification/NotificationSearchBar.tsx
+++ frontend/altinn-support-dashboard.client/src/components/Notification/NotificationSearchBar.tsx
@@ -3,25 +3,27 @@ import { Button, Search, Textfield } from "@digdir/designsystemet-react";
 import style from "./styles/NotificationSearchBar.module.css";
 
 type NotificationSearchBarProps = {
-  orderId: string;
-  setOrderId: (value: string) => void;
+  searchValue: string;
+  setSearchValue: (value: string) => void;
+  searchType: "orderId" | "nin";
 };
 
 const NotificationSearchBar: React.FC<NotificationSearchBarProps> = ({
-  orderId,
-  setOrderId,
+  searchValue,
+  setSearchValue,
+  searchType
 }) => {
-  const [inputValue, setInputValue] = useState(orderId ?? "");
+  const [inputValue, setInputValue] = useState(searchValue ?? "");
 
   const handleSearch = () => {
-    setOrderId(inputValue);
+    setSearchValue(inputValue);
   };
 
   return (
     <div className={style.container}>
       <Textfield
-        label="Order ID"
-        placeholder="Skriv inn ordre-id"
+        label={searchType === "orderId" ? "Ordre-ID" : "NIN"}
+        placeholder={searchType === "orderId" ? "Skriv inn ordre-ID" : "Skriv inn NIN"}
         value={inputValue}
         onChange={(e) => setInputValue(e.target.value)}
         onKeyDown={(e) => {
@@ -41,7 +43,7 @@ const NotificationSearchBar: React.FC<NotificationSearchBarProps> = ({
       <Button
         onClick={() => {
           setInputValue("");
-          setOrderId("");
+          setSearchValue("");
         }}
         className={style.removeButton}
       >
--- frontend/altinn-support-dashboard.client/src/pages/NotificationPage.tsx
+++ frontend/altinn-support-dashboard.client/src/pages/NotificationPage.tsx
@@ -1,35 +1,60 @@
-import { Heading } from "@digdir/designsystemet-react";
+import { Heading, ToggleGroup } from "@digdir/designsystemet-react";
 import { useEffect, useState } from "react";
 import NotificationSearchBar from "../components/Notification/NotificationSearchBar";
 import { useNotifications } from "../hooks/hooks";
 import NotificationCard from "../components/Notification/NotificationCard";
 import style from "./styles/NotificationPage.module.css";
 import { showPopup } from "../components/Popup";
+import NotificationShipmentCard from "../components/Notification/NIN search/NotificationShipmentCard";
+
+type SearchType = "orderId" | "nin";
 
 export const NotificationPage = () => {
-  const [orderId, setOrderId] = useState("");
-  const notificationQuery = useNotifications(orderId);
+  const [searchType, setSearchType] = useState<SearchType>("orderId");
+  const [searchValue, setSearchValue] = useState("");
+
+  const orderQuery = useNotifications(searchType === "orderId" ? searchValue : "");
+  const ninQuery = "";
+
+  const activeQuery = searchType === "orderId" ? orderQuery : ninQuery;
 
   useEffect(() => {
-    if (notificationQuery.isError) {
-      showPopup(notificationQuery.error.message, "error");
+    if (activeQuery.isError) {
+      showPopup(activeQuery.error.message, "error");
     }
-  }, [notificationQuery]);
+  }, [activeQuery]);
+
   return (
     <div className={style.container}>
       <Heading level={1} data-size="sm" className={style.heading}>
         Søk etter varsling
       </Heading>
 
-      <NotificationSearchBar orderId={orderId} setOrderId={setOrderId} />
+      <ToggleGroup
+        value={searchType}
+        onChange={(val) => { setSearchType(val as SearchType); setSearchValue(""); }}
+        data-size="sm"
+      >
+        <ToggleGroup.Item value="orderId">Order-Id</ToggleGroup.Item>
+        <ToggleGroup.Item value="nin">NIN</ToggleGroup.Item>
+      </ToggleGroup>
+
+      <NotificationSearchBar
+        searchValue={searchValue}
+        setSearchValue={setSearchValue}
+        searchType={searchType}
+      />
 
       {/* Filters out the notifications with 0 (shows only email if sms was 0 f.ex.) */}
-      {notificationQuery.data
-        ?.filter((o) => o.notifications.length > 0)
-        .map((order, index) => (
-          <div key={index}>
-            <NotificationCard key={index} order={order} />
-          </div>
+      {/* Different result view based on what type of search it is */}
+      {searchType === "orderId" && 
+        orderQuery.data
+          ?.filter((o) => o.notifications.length > 0)
+          .map((order, i) => <NotificationCard key={i} order={order}/>)}
+
+      {searchType === "nin" &&
+        ninQuery.data?.map((shipment, i) => (
+          <NotificationShipmentCard key={i} shipment={shipment}/> 
         ))}
     </div>
   );
