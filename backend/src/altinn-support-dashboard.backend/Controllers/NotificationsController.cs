da257bd649fcc0e9f7257a20f6e135e549f806c7
ugedal1 <ugedal1@hotmail.com>
2026-07-08 12:15:06 +0200

    feat: made generic future endpoint
    - Made this endpoint so validation stays in the backend and minimizes
      frontend implementation

--- backend/src/altinn-support-dashboard.backend/Controllers/NotificationsController.cs
+++ backend/src/altinn-support-dashboard.backend/Controllers/NotificationsController.cs
@@ -84,6 +84,24 @@ public class NotificationsController : ControllerBase
         return Ok(response);
     }
 
+    [HttpGet("future/{query}")]
+    public async Task<IActionResult> GetFutureNofifications([FromRoute] string environmentName,
+        [FromRoute] string query,
+        [FromQuery] DateTime? from,
+        [FromQuery] DateTime? to)
+    {
+        if (ValidationService.IsValidOrgNumberV2(query))
+        {
+            return await GetFutureNotificationsByOrgNr(environmentName, query, from, to);
+        }
+
+        if (ValidationService.isValidSsn(query))
+        {
+            return await GetFutureNotificationsByNin(environmentName, query, from, to);
+        }
+        return BadRequest("Not a valid nin or org number");
+    }
+
     [HttpPost("availability")]
     public async Task<IActionResult> GetNotificationAvailabilityForResource([FromRoute] string environmentName, [FromBody] NotificationAvailabilityRequest request)
     {
