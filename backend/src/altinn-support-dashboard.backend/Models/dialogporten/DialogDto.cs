3115fef347b7ab8bffaebae0d227d6fadfaaa0f3
ugedal1 <ugedal1@hotmail.com>
2026-08-14 10:20:39 +0200

    feat: added dialogDto

---
+++ backend/src/altinn-support-dashboard.backend/Models/dialogporten/DialogDto.cs
@@ -0,0 +1,34 @@
+namespace Models.dialogporten;
+
+public class DialogDto
+{
+	public required string DialogId { get; set; }
+	public required string InstanceRef { get; set; }
+	public required string Party { get; set; }
+	public required ServiceResourceDto ServiceResource { get; set; }
+	public required ServiceOwnerDto ServiceOwner { get; set; }
+	public required List<ResourceNameDto> Title { get; set; }
+	public required List<ResourceNameDto> NonSensetiveTitle { get; set; }
+
+}
+
+public class ServiceResourceDto
+{
+	public required string Id { get; set; }
+	public required bool IsDelegable { get; set; }
+	public required int MinimumAuthenticationLevel { get; set; }
+	public required List<ResourceNameDto> Name { get; set; }
+
+}
+public class ResourceNameDto
+{
+	public required string Value { get; set; }
+	public required string LanguageCode { get; set; }
+}
+public class ServiceOwnerDto
+{
+	public required string OrgNumber { get; set; }
+	public required string Code { get; set; }
+	public required List<ResourceNameDto> Name { get; set; }
+
+}
