c00d0a8065ba48632ac297732893d00f4618c8b3
ugedal1 <ugedal1@hotmail.com>
2026-08-14 09:11:37 +0200

    feat: added client
    - Added dialogportent client with corresponding interface
    - Added basic GetDialogById function

---
+++ backend/src/altinn-support-dashboard.backend/Clients/DialogportenClient.cs
@@ -0,0 +1,43 @@
+using altinn_support_dashboard.Server.Models;
+using Microsoft.Extensions.Options;
+
+public class DialogportenClient : IDialogportenClient
+{
+    private readonly Dictionary<string, HttpClient> _clients = new();
+    private readonly ILogger<INotificationsClient> _logger;
+
+    public DialogportenClient(IOptions<Configuration> configuration, IHttpClientFactory clientFactory, ILogger<INotificationsClient> logger)
+    {
+        _logger = logger;
+        InitClient(nameof(configuration.Value.TT02), configuration.Value.TT02, clientFactory);
+        InitClient(nameof(configuration.Value.Production), configuration.Value.Production, clientFactory);
+    }
+
+    private void InitClient(string environmentName, EnvironmentConfiguration config, IHttpClientFactory clientFactory)
+    {
+        var client = clientFactory.CreateClient(environmentName);
+        client.DefaultRequestHeaders.Add("Ocp-Apim-Subscription-Key", config.Ocp_Apim_Subscription_Key);
+        client.BaseAddress = new Uri(config.BaseAddressAltinn3);
+        client.Timeout = TimeSpan.FromSeconds(config.Timeout);
+        client.DefaultRequestHeaders.Add("ApiKey", config.ApiKey);
+        _clients.Add(environmentName, client);
+    }
+
+    public async Task<string> GetDialogById(string urn, string environmentName)
+    {
+        var client = _clients[environmentName];
+        var response = await client.GetAsync($"/api/v1/serviceowner/dialoglookup?instanceRef={urn}");
+        var responseBody = await response.Content.ReadAsStringAsync();
+
+        if (!response.IsSuccessStatusCode)
+        {
+            throw new HttpRequestException(
+                $"Api request failed with status code {response.StatusCode}: {responseBody}",
+                inner: null,
+                statusCode: response.StatusCode);
+        }
+
+        return responseBody;
+    }
+
+}
